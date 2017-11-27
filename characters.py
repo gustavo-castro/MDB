ac3c48ed6328386dfa8a3a76cc4d371771b594fc
gustavo <gustavo.pdcastro@gmail.com>
2017-11-28 00:15:58 +0100

    Improving walking and collisions

--- characters.py
+++ characters.py
@@ -42,7 +42,6 @@ class Character(pygame.sprite.Sprite):
 
     def fixPosition(self, hit_list, walking_direction):
         if hit_list:
-            self.cover = True
             if walking_direction == 1:
                 closest_one = hit_list[0].rect.right
             elif walking_direction == 2:
@@ -61,28 +60,73 @@ class Character(pygame.sprite.Sprite):
                     closest_one = hitted.rect.bottom
                 elif walking_direction == 4 and hitted.rect.top < closest_one:
                     closest_one = hitted.rect.top
+            previouswidth, previousheight = self.image.get_rect().width, self.image.get_rect().height
             if walking_direction == 1:
                 self.x = closest_one
-                self.rect.x = self.x
                 self.image = self.imagedict['cr']
+                self.rect = self.image.get_rect()
+                if self.cover:
+                    if self.blocked_direction == 3:
+                        self.y += self.rect.height - previousheight
+                    elif self.blocked_direction == 4:
+                        self.y -= self.rect.height - previousheight
+                [self.rect.x, self.rect.y] = [self.x, self.y]
             elif walking_direction == 2:
-                self.x = closest_one - self.rect.width
-                self.rect.x = self.x
+                self.x = closest_one
                 self.image = self.imagedict['cl']
+                self.rect = self.image.get_rect()
+                self.x -= self.rect.width
+                if self.cover:
+                    if self.blocked_direction == 3:
+                        self.y += self.rect.height - previousheight
+                    elif self.blocked_direction == 4:
+                        self.y -= self.rect.height - previousheight
+                [self.rect.x, self.rect.y] = [self.x, self.y]
             elif walking_direction == 3:
                 self.y = closest_one
-                self.rect.y = self.y
                 self.image = self.imagedict['cd']
+                if self.cover:
+                    if self.blocked_direction == 1:
+                        self.x -= self.rect.width - previouswidth
+                    if self.blocked_direction == 2:
+                        self.x += self.rect.width - previouswidth
+                self.rect = self.image.get_rect()
+                [self.rect.x, self.rect.y] = [self.x, self.y]
             elif walking_direction == 4:
-                self.y = closest_one - self.rect.height
-                self.rect.y = self.y
+                self.y = closest_one
                 self.image = self.imagedict['cu']
+                self.rect = self.image.get_rect()
+                self.y -= self.rect.height
+                if self.cover:
+                    if self.blocked_direction == 1:
+                        self.x -= self.rect.width - previouswidth
+                    if self.blocked_direction == 2:
+                        self.x += self.rect.width - previouswidth
+                [self.rect.x, self.rect.y] = [self.x, self.y]
+            self.cover = True
+            self.blocked_direction = walking_direction
+        else:
+            if self.cover:
+                if self.blocked_direction in [1,2]:
+                    if walking_direction in [3,4]:
+                        return
+                    else:
+                        previousheight = self.image.get_rect().height
+                        self.image = self.imagedict[self.currentdirection][self.feettonum[self.feetleft]]
+                        self.rect = self.image.get_rect()
+                        [self.rect.x, self.rect.y] = [self.x, self.y]
+                        aux_hitted = self.checkwallcollision()+self.checkcharactercollision()
+                        if aux_hitted:
+                            self.y -= self.rect.height - previousheight
+                            self.rect.y = self.y
+                elif self.blocked_direction in [3,4]:
+                    if walking_direction in [1, 2]:
+                        return
+            self.cover = False
 
     def updatePosition(self, eventkey):
         if eventkey in [K_a, K_d, K_w, K_s]:
             self.feetleft = not self.feetleft
-            self.image = self.imagedict[self.currentdirection][self.feettonum[self.feetleft]]
-            self.cover = False
             if eventkey == K_a:
                 self.x -= self.walksize_x
                 self.rect.x = self.x
@@ -99,6 +143,10 @@ class Character(pygame.sprite.Sprite):
                 self.y += self.walksize_y
                 self.rect.y = self.y
                 walking_direction = 4
+            if not self.cover:
+                self.image = self.imagedict[self.currentdirection][self.feettonum[self.feetleft]]
+                self.rect = self.image.get_rect()
+                [self.rect.x, self.rect.y] = [self.x, self.y]
         else:
             return
         self.fixPosition(self.checkwallcollision()+self.checkcharactercollision(), walking_direction)
